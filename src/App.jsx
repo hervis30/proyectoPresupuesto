9287337852d6b2a8586b93bc012260f215e4bf57
hervis30 <hervisjonat@gmail.com>
2022-06-20 17:46:37 -0500

    se inicializa proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+import "./App.css";
+import ComponentePrincipal from './components/ComponentePrincipal';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <ComponentePrincipal />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ComponentePrincipal.jsx
@@ -0,0 +1,12 @@
+// en este componente definimos los useState, funciones, etc,
+//que necesitamos desplegar en todo el proyecto.
+import React from 'react';
+
+
+const ComponentePrincipal = () => {
+    return (
+        <div>ComponentePrincipal</div>
+    )
+}
+
+export default ComponentePrincipal;
