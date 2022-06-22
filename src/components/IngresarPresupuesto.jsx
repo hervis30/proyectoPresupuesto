f3ebc67c5394ea4646d8ee3be292d768be6c2472
Carlos Ortega <carlosalbertoortegac@.com>
2022-06-21 21:43:35 -0500

    budget start

---
+++ src/components/IngresarPresupuesto.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const IngresarPresupuesto = () => {
+    return (
+        <div className='ingresarPresupuesto'>
+            <div>
+                <h2>Ingresar Presupuesto</h2>
+                <input type="number" className='ingresainput' />
+            </div>
+            <button type='submit'></button>
+        </div>
+    )
+}
+
+export default IngresarPresupuesto
