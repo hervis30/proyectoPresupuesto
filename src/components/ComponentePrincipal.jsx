3ba2a964f48e8fec2d2951d12b562e6f060cd759
alexander <alexaruba18@gmail.com>
2022-06-21 20:12:18 -0500

    se crea rama planificadorGastos

--- src/components/ComponentePrincipal.jsx
+++ src/components/ComponentePrincipal.jsx
@@ -1,11 +1,16 @@
 // en este componente definimos los useState, funciones, etc,
 //que necesitamos desplegar en todo el proyecto.
 import React from 'react';
+import PlanificadorGastos from './PlanificadorGastos';
 
 
 const ComponentePrincipal = () => {
+    
+
     return (
-        <div>ComponentePrincipal</div>
+        <>
+        <PlanificadorGastos/>
+        </>
     )
 }
 
---
+++ src/components/PlanificadorGastos.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const PlanificadorGastos = () => {
+  return (
+    <>
+        <div className="planificadorPadre">
+            <h1>Planificador de Gastos</h1>
+            <div className="planificadorHijo">
+                <div className="cake">
+
+                </div>
+                <div className="presupuestoGastosDisponible">
+                        <h4>Total Presupuesto:</h4>
+                        <h4>Disponible:</h4>
+                        <h4>Gastado:</h4>
+                </div>
+            </div>
+        </div>
+    
+    
+    </>
+  )
+}
+
+export default PlanificadorGastos
