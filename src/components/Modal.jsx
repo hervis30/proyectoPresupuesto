8e488be0178643c9997a8687c12ada41c0ed2b73
Carlos Ortega <carlosalbertoortegac@.com>
2022-06-22 23:18:04 -0500

    update from commit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -20,19 +20,21 @@ const Modal = () => {
     return (
         <Fragment>
             <div>
-                
+                {/*boton de abrir modal */}
                 <button className="BtnModal"
                     onClick={AbrirModal}
                 >
                 Agregar Gasto
                 </button>
                 
+                {/*funcion para mostrar modal */}
                 {modal && (
                     <div className="modal">
                         <div onClick={AbrirModal} className="overlay"></div>
+                        
                         <div className="modal-content">
                             <h2>Agregar Gastos</h2>
-                            
+                            {/*formulario del modal */}
                             <form className="FormularioGasto">
                                 <label>Nombre Gasto</label>
                                 <input type="text"
@@ -52,12 +54,13 @@ const Modal = () => {
                                     <option value="6">Tecnologia</option>
                                 </select>
                             </form>
-                            
+                            {/* boton de cerrar el modal */}
                             <p className="close-modal" 
                             onClick={AbrirModal}
                             >
                             X
                             </p>
+                            {/* boton para enviar los datos al planificador de gastos */}
                             <button type="submit" 
                             className="BntAgregarGasto"
                             >Agregar Gastos
